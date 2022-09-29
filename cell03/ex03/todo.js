fe4d92c7c9f52be571ff855637ffed6ebd091024
Nazza01 <nate_thomas_95@hotmail.com>
2022-09-29 20:30:59 +0930

    Playing around with localstorage, not there yet

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -1,19 +1,37 @@
-function newItem() {
-	var place = document.getElementById("todo-list");
-	var div = document.createElement("div");
-	var todo_item = prompt("Please enter the todo item");
-	
-	if (todo_item != null) {
-		div.className = "item";
-		div.innerHTML = todo_item.replace(/</g, "&lt;");
-		div.addEventListener("click", deleteItem);
-		place.appendChild(div);
-		function deleteItem() {
-			var conf = confirm("Would you like to delete this item?");
-			if (conf == true) {
-				place.removeChild(this);
-			}
-		}
-	}
+var index = 0;
 
+window.onload = function loadcache() {
+	for (var i = 0; i < localStorage.length; i++) {
+		addItem(localStorage.getItem(localStorage.key(i)));
+	}
+}
+
+function addItem(text) {
+	var div = document.createElement("div");
+	var place = document.getElementById("todo-list");
+	
+	div.className = "item";														//	<div class="item"></div>
+	div.innerHTML = text;														//	Whatever you write in the prompt will be replaced here: <div class="item">todo_item</div>
+	div.addEventListener("click", deleteItem);									//	Add the deletion button event listener when the item is clicked
+	place.appendChild(div);														//	Add the div element into the correct place (under the todo-list div)
+	localStorage.setItem(String(index), text);
+	index++;
+}
+
+function newItem() {
+	var todo_item = prompt("Please enter the todo item");
+	if (todo_item != null) {
+		addItem(todo_item);
+	}
+}
+
+function deleteItem() {
+	var conf = confirm("Would you like to delete this item?");
+	var place = document.getElementById("todo-list");
+
+	if (conf == true) {
+		localStorage.removeItem(localStorage.getItem(String(index)));			//	Remove the localstorage item
+		place.removeChild(this);												//	Remove the div element
+		index--;																//	Decrement the index
+	}
 }
